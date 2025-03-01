61f7e3b464d9225bb77d6e937df0ba437000d65d
caetanojpo <106156212+caetanojpo@users.noreply.github.com>
2025-03-01 19:22:52 -0300

    feat(config): created mariadb config

---
+++ src/infrastructure/config/mariadb.config.service.ts
@@ -0,0 +1,21 @@
+import { TypeOrmModuleOptions, TypeOrmOptionsFactory } from '@nestjs/typeorm';
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+
+@Injectable()
+export class MariaDBConfigService implements TypeOrmOptionsFactory {
+  constructor(private readonly configService: ConfigService) {}
+
+  createTypeOrmOptions(): TypeOrmModuleOptions {
+    return {
+      type: 'mariadb',
+      host: this.configService.get<string>('DB_HOST'),
+      port: parseInt(this.configService.get<string>('DB_PORT') || '3306'),
+      username: this.configService.get<string>('DB_USER'),
+      password: this.configService.get<string>('DB_PASS'),
+      database: this.configService.get<string>('DB_NAME'),
+      entities: [], // Add your entities here
+      synchronize: true,
+    };
+  }
+}
