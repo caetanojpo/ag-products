4b7f67ddf4cec58be7f2995755180d4cb17c44df
caetanojpo <106156212+caetanojpo@users.noreply.github.com>
2025-03-01 19:23:03 -0300

    feat(config): updated app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,17 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from './modules/user.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { MariaDBConfigService } from './infrastructure/config/mariadb.config.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [UserModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync({
+      useClass: MariaDBConfigService,
+      inject: [MariaDBConfigService],
+    }),
+    UserModule,
+  ],
 })
 export class AppModule {}
