e9020901ca1dd077283c942e1abca3e5858bd43b
caetanojpo <106156212+caetanojpo@users.noreply.github.com>
2025-03-03 13:59:49 -0300

    feat(user): updated user module

--- src/modules/user.module.ts
+++ src/modules/user.module.ts
@@ -1,8 +1,14 @@
 import { Module } from '@nestjs/common';
+import { UserController } from '../presentation/controllers/user.controller';
+import { CreateUserUsecase } from '../application/usecases/users/create-user.usecase';
+import { AuthUsecase } from '../application/usecases/auth/auth-usecase';
+import { PersistenceUserRepository } from '../infrastructure/database/repository/persistence-user.repository';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserSchema } from '../infrastructure/database/persistence/schemas/user.schema';
 
 @Module({
-  imports: [],
-  controllers: [],
-  providers: [],
+  imports: [TypeOrmModule.forFeature([UserSchema])],
+  controllers: [UserController],
+  providers: [CreateUserUsecase, AuthUsecase, PersistenceUserRepository],
 })
 export class UserModule {}
